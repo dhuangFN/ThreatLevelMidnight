be82f15f804a87b85efac968fe3f506da3270ca1
Sam North <sam.north@fiscalnote.com>
2023-03-30 16:00:22 -0500

    fully functioning UI to api

--- threat-level-midnight-au-ui/src/app.js
+++ threat-level-midnight-au-ui/src/app.js
@@ -1,8 +1,36 @@
 export class App {
   message = 'Hello World!';
   prompt;
+  result;
 
   doStuff(){
+    
+    const url = 'http://localhost:3000';
+    const method = 'POST';
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    const bodyData = JSON.stringify({
+      prompt: this.prompt
+    });
 
+    fetch(url, {
+      method: method,
+      headers: headers,
+      body: bodyData
+    })
+      .then((response) => {
+        // if (!response.ok) {
+        //   throw new Error('Network response was not ok');
+        // }
+        return response.json();
+      })
+      .then((data) => {
+        this.result = data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
   }
 }
