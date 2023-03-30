e8e9e60d6f68e88a41a3fc262d79a36bf31edcb9
David Huang <david.huang@fiscalnote.com>
2023-03-30 13:47:18 -0500

    add openai library

--- threat-level-midnight/src/app.service.ts
+++ threat-level-midnight/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+const { Configuration, OpenAIApi } = require("openai");
 
 @Injectable()
 export class AppService {
@@ -16,5 +17,21 @@ export class AppService {
         'sentiment-reason': 'well.  there was a lot of aggressive stuff in there.'
       }
     };
+  }
+
+  async getThreatLevel(prompt: string) : Promise<any> {
+      const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+      });
+  
+      const openai = new OpenAIApi(configuration);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0,
+        max_tokens: 7,
+      });
+  
+      return response.data;
   }
 }
